38a71b63d3bfae7ec97b027a54fca3213ab35986
Nafyad <nafyadhailu99@gmail.com>
2025-03-24 20:57:53 +0300

    add user get,put,patch

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,6 @@
 """views for user API"""
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -15,7 +15,20 @@ class CreateUserView(generics.CreateAPIView):
     """ Create a new user in the system"""
     serializer_class = UserSerializer
 
+
 class CreateTokenView(ObtainAuthToken):
     """create new auth token for user"""
     serializer_class= AuthTokenSerializer
     renderer_classes= api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage te authenticated user"""
+    serializer_class= UserSerializer
+    authentication_classes= [authentication.TokenAuthentication]
+    permission_classes= [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user"""
+        return self.request.user
+
+
