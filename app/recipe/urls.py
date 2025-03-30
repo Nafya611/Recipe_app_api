476c69022ae486e8495cefa1dda8c21b12f92469
Nafyad <nafyadhailu99@gmail.com>
2025-03-30 19:19:41 +0300

    Add CRUD operations for recipe by ID: GET, PUT, DELETE

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,6 +1,7 @@
 from django.urls  import path,include
-from .views import recipe_list
+from .views import recipe_list,recipe_detail
 
 urlpatterns=[
-    path('recipes/', recipe_list, name='recipe-list')
+    path('recipes/', recipe_list, name='recipe-list'),
+    path('recipes/<int:pk>', recipe_detail, name= 'recipe-detail')
 ]
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from drf_spectacular.utils import extend_schema
-from recipe.serializers import RecipeSerializer
+from drf_spectacular.utils import extend_schema, OpenApiResponse
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 from core.models import Recipe
+from django.shortcuts import get_object_or_404
 
 @extend_schema(
     methods=['POST'],
@@ -17,7 +18,10 @@ from core.models import Recipe
 @extend_schema(
     methods=['GET'],
     responses={200: RecipeSerializer(many=True)}  # Define response for GET
+
 )
+
+
 @api_view(['POST', 'GET'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
@@ -36,3 +40,37 @@ def recipe_list(request):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@extend_schema(
+    methods=['PUT'],
+    description="Update an existing recipe by providing all required fields.",
+    request=RecipeDetailSerializer,
+    responses={
+        200: RecipeDetailSerializer,
+        400: OpenApiResponse(description="Bad request")
+    },
+)
+@api_view(['PUT','GET','DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def recipe_detail(request,pk):
+    """handle detail of recipe in id"""
+    try:
+        recipe = Recipe.objects.get(pk=pk, user=request.user)
+    except Recipe.DoesNotExist:
+        return Response({"error": "Recipe not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer =  RecipeDetailSerializer(recipe)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer =RecipeDetailSerializer(recipe,data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return Response({"message":"Recipe deleted successfully."},status=status.HTTP_204_NO_CONTENT)
