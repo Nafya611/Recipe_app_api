9602a87d5a5f23af8948dcf6248b5e0fa9e6a493
Nafyad <nafyadhailu99@gmail.com>
2025-04-10 11:03:17 +0300

    modify partial update for the image uploading

--- app/app/urls.py
+++ app/app/urls.py
@@ -39,10 +39,6 @@ urlpatterns = [
      path('api/recipe/', include('recipe.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root= settings.MEDIA_ROOT
-    )
+
 
 
